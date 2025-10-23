743f3ca7f2520140199d9c512becf86214a3d1ec
Marina Kramarchuk <kramarchukmm20182018@gmail.com>
2025-10-23 17:20:16 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,50 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+
+export type FieldsForSelect = boolean | null;
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [selectedField, setSelectedField] = useState<FieldsForSelect>(null);
+  const [query, setQuery] = useState<string>('');
+
+  const prepareTodos = (field: FieldsForSelect, text: string): Todo[] => {
+    let newArray = [...todos];
+
+    if (field !== null) {
+      newArray = newArray.filter(todo => todo.completed === field);
+    }
+
+    if (text) {
+      newArray = newArray.filter(todo =>
+        todo.title
+          .trim()
+          .toLocaleLowerCase()
+          .includes(text.toLocaleLowerCase()),
+      );
+    }
+
+    return newArray;
+  };
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .catch(() => {
+        throw new Error('Error');
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +53,23 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                onSetSelectedField={setSelectedField}
+                onSetQuery={setQuery}
+                query={query}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading ? (
+                <Loader />
+              ) : (
+                <TodoList todos={prepareTodos(selectedField, query)} />
+              )}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
     </>
   );
 };
